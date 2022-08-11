4a811d75f27c37e1006a30d824c7e562886e4f43
Xin Ruan <xin.ruan@sap.com>
2022-08-11 21:44:22 +0200

    update helmchart

--- manifests-template/generate-module-template.sh
+++ manifests-template/generate-module-template.sh
@@ -26,24 +26,25 @@ SIGNED_PATH="signed"
 MODULE_TEMPLATE="./generated-module-template.yaml"
 MODULE_TEMPLATE_CHANNEL="stable"
 MODULE_NAME="kyma-project.io/module/manifest1"
-MODULE_VERSION="v0.0.56"
+MODULE_VERSION="v0.0.61"
 MODULE_PROFILE="production"
 
 # this requires a k3d registry with a cluster
 # e.g. k3d cluster create operator-test --registry-create operator-test-registry.localhost:50241
 REGISTRY_NAME="operator-test-registry"
-#REGISTRY_HOST="ghcr.io"
-REGISTRY_HOST="https://europe-west3-docker.pkg.dev"
+REGISTRY_HOST="ghcr.io"
+#REGISTRY_HOST="https://europe-west3-docker.pkg.dev"
 #REGISTRY_HOST="localhost"
 REGISTRY_URL="${REGISTRY_HOST}/ruanxin"
-REGISTRY_URL="${REGISTRY_HOST}/sap-kyma-jellyfish-dev/kyma-operator"
+#REGISTRY_URL="${REGISTRY_HOST}/sap-kyma-jellyfish-dev/kyma-operator"
 #REGISTRY_URL="${REGISTRY_HOST}:5000"
 CHART_NAME="kyma-load-test"
 PASSWORD=ya29.c.b0AXv0zTNbhYWunpyTtQXJ_-BXLcryi6SQkJqForyljqLsrnqVt7K6JF6tluh2uwZi3Cr9IZUGZmI9D76OaA3jza7VQqXfrLOOxyU-7l73Nyxxu_7TaZ5iPUr6iY4H3c9sxvPlOioLKw0hYxOprZl21fkX-mUttNhxKMRWv6Q3VrmiCtGZA5dhh9cNtYLSB6SryyBLqL-xbf3glVFLlcpEITxYLtbG-28R1BthyXI5AIvWRrVfuEbG20Gys3cF3pQZsJJehZhApGXd1I38wv5zIvPijjq9TM6ODjXyDOm6wETjouPVm6AZt3XW_x5FeU_qefr6tcONArdQE-lMfSID1hiC5d6PbLKAfeQu-VR0W3UnjYCOiM9X5Qiom2vMuZyXKrSFSDOMUdOyuUCkLK_qmPUOOCKtEQSrBtnU7f950TLJeNNIUSDdXiv0e0nkEWFEdmP0Yg5pB1qVCuW-qA
 
 rm -rf "${DATA_DIR}/${CHART_NAME}"
+helm repo update
 helm repo add load-test-charts https://ruanxin.github.io/performance-test
-helm pull "load-test-charts/${CHART_NAME}" --version=0.4.0 --untar --untardir ${DATA_DIR}
+helm pull "load-test-charts/${CHART_NAME}" --version=0.5.0 --untar --untardir ${DATA_DIR}
 
 #if k3d registry get ${REGISTRY_NAME} | grep -q ${REGISTRY_NAME}; then
 #   echo "OCI Registry ${REGISTRY_NAME} Exists, continuing..."
@@ -63,8 +64,8 @@ rm -r ${COMPONENT_ARCHIVE}
 
 component-cli component-archive create ${COMPONENT_ARCHIVE} --component-name ${MODULE_NAME} --component-version ${MODULE_VERSION}
 component-cli ca resources add ${COMPONENT_ARCHIVE} ${COMPONENT_RESOURCES}
-#component-cli ca remote push -v9 ${COMPONENT_ARCHIVE} --repo-ctx ${REGISTRY_URL}
-component-cli ca remote push -v9 --username oauth2accesstoken --password ${PASSWORD} ${COMPONENT_ARCHIVE} --repo-ctx ${REGISTRY_URL}
+component-cli ca remote push -v9 ${COMPONENT_ARCHIVE} --repo-ctx ${REGISTRY_URL}
+#component-cli ca remote push -v9 --username oauth2accesstoken --password ${PASSWORD} ${COMPONENT_ARCHIVE} --repo-ctx ${REGISTRY_URL}
 
 rm ${PRIVATE_KEY}
 openssl genpkey -algorithm RSA -out ${PRIVATE_KEY}
